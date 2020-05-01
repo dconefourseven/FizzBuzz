d161e7fb8654f3855466b1b153606a9a991f8306
David Clarke <davidclarke1990@gmail.com>
2020-05-01 10:14:49 +0100

    Adding FizzBuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        // ---- Variables ----
+
+        private int m_countTarget = 0;
+
+        // ---- Constructor ----
+
+        public FizzBuzz(int _target)
+        {
+            m_countTarget = _target;
+        }
+
+        // ---- Methods ----
+
+        public void DoWork()
+        {
+            int index = 1;
+            while (index <= m_countTarget)
+            {
+                if (((index % 3) == 0) && ((index % 5) == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ((index % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((index % 5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine("{0}", index);
+                }
+
+                ++index;
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin FizzBuzz!");
+
+            var fizzbuzzer = new FizzBuzz(100);
+            fizzbuzzer.DoWork();
+
+            Console.WriteLine("End FizzBuzz.");
+        }
+    }
+}
