c2107cd0ab412c8f1915be0d3fa7c664aae19b53
David Clarke <davidclarke1990@gmail.com>
2020-05-01 10:26:59 +0100

    Timing the execution

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace FizzBuzz
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(/*string[] args*/)
         {
             Console.WriteLine("Begin FizzBuzz!");
+            Stopwatch stopwatch = new Stopwatch();
 
+            stopwatch.Start();
             var fizzbuzzer = new FizzBuzz(100);
             fizzbuzzer.DoWork();
+            stopwatch.Stop();
 
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
             Console.WriteLine("End FizzBuzz.");
+            
+            bool endProgram = false;
+            while (!endProgram)
+            {
+                Console.WriteLine("Enter Y to continue...");
+                string line = Console.ReadLine();
+                endProgram = String.Compare(line, "Y", true) == 0;
+            }
         }
     }
 }
